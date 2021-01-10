d00d8002d96f6b380a182823c742a8db86c1beec
Evan Doherty <edoherty77@gmail.com>
2021-01-09 23:24:22 -0500

    styling and ability to add more than one item

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,9 +3,24 @@ var stripe = Stripe(
   'pk_test_51I7XskGD6wn1FE2sFBOYFDTjEDELj9w1QsgWwPqyJNmQJF1S2GTP9etucoaBhUxYk0Y0g2tibWxDFfn8IMoWQl7K00Uqua01Dg',
 )
 var checkoutButton = document.getElementById('checkout-button')
-// var testBtn = document.getElementById('test-btn')
+var list = document.getElementById('ul')
+var addBtn = document.getElementById('add-button')
 var size = document.getElementById('size')
+let num = 1
 var quantity = document.getElementById('quantity')
+var row = document.getElementById('selection-row')
+
+addBtn.addEventListener('click', function () {
+  num = num + 1
+  let newLi = document.createElement('li')
+  newLi.innerHTML = row.innerHTML
+  newLi.classList.add('more', 'li')
+  // newLi.setAttribute('class', 'more')
+  newLi.setAttribute('id', num)
+  // newLi.append(row)
+  // newLi.append(li)
+  list.append(newLi)
+})
 
 checkoutButton.addEventListener('click', function () {
   let details = {
